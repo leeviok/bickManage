f50ce52bdbee5229b94c04f3e4db784265563a30
wei li <oneaspirer@163.com>
2019-01-25 17:59:52 +0800

    add content container

--- my_app/src/admin.js
+++ my_app/src/admin.js
@@ -3,7 +3,9 @@ import { Row, Col } from 'antd'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import NavLeft from './components/NavLeft'
+import Home from './pages/home'
 import './style/common.less'
+
 export default class Admin extends React.Component {
   render () {
     return (
@@ -14,7 +16,7 @@ export default class Admin extends React.Component {
         <Col span={21} className="main">
           <Header />
           <Row className="content">
-            content
+            <Home />
           </Row>
           <Footer />
         </Col>
